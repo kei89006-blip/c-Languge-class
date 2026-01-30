5a69a95f77c2492d0373c18440d6394300a7cea8
keigo <kei89006@gmail.com>
2026-01-30 19:09:41 +0900

    add kadai11_6.c

---
+++ kadai11_6.c
@@ -0,0 +1,18 @@
+	
+/*
+program=kadai11_6.c
+date=1月19日
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    for(int i=argc-1; i>=1; i--){
+        printf("%s ",  argv[i]);
+    }
+    printf("\n");
+    return 0;
+}
+/*
+carrot banana apple
+*/
